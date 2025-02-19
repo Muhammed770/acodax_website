2c604162878997de63e1a9891479d2d3528642b3
muhammed770 <mail.muhammed2002@gmail.com>
2025-02-19 10:34:33 +0400

    dt bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
   return (
     <div className="relative z-10 flex items-center justify-center h-screen">
       <Header1 />
-      <div className="flex-col items-center justify-center px-2">
-        <div className=" w-full dark:bg-black bg-white  dark:bg-dot-white/[0.4] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div className="h-full w-full    dark:bg-dot-white/[0.3] bg-dot-black/[0.2] relative flex items-center justify-center">
+        <div className="flex-col items-center justify-center px-2">
           <div className="text-center">
             {/* Radial gradient for the container to give a faded look */}
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
