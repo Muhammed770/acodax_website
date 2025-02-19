19097caee01fd53ddf5f67f2d24c6686a28bfd99
muhammed770 <mail.muhammed2002@gmail.com>
2025-02-19 09:15:22 +0400

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { DotPattern } from "@/components/magicui/dot-pattern";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { Header1 } from "@/components/navbar";
+import { ShinyButton } from "@/components/magicui/shiny-button";
+import { MoveRight } from "lucide-react";
+import BlurFade from "@/components/magicui/blur-fade";
+import { Logos8 } from "@/components/trusted-by-logos";
+import { TextAnimate } from "@/components/magicui/text-animate";
 
+
+const BLUR_FADE_DELAY = 0.04;
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="relative z-10 flex items-center justify-center h-screen">
+      <Header1 />
+      <div className="flex-col items-center justify-center ">
+        <div className="text-center">
+          <DotPattern
+            glow={true}
+            opacity={0.7}
+            className={cn(
+              "[mask-image:radial-gradient(700px_circle_at_center,white,transparent)]",
+            )}
+          />
+          <div className="flex flex-col items-center justify-center">
+            <BlurFadeText delay={BLUR_FADE_DELAY} className="text-6xl " text="Brace yourselves for the future of" animateByCharacter={true} />
+            <BlurFadeText delay={BLUR_FADE_DELAY} className="text-6xl " text=" business intelligence" animateByCharacter={true} />
+          </div>
+          <div className="flex flex-col items-center justify-center m-4">
+            <BlurFade delay={BLUR_FADE_DELAY * 3} >
+              <h3 className="text-md text-white/40 whitespace-pre-wrap"> 
+                The all-in-one ERP solution designed for businesses that demand 
+                <br />
+                efficiency and scalability.</h3>
+            </BlurFade>
+          </div>
+          <BlurFade delay={BLUR_FADE_DELAY * 5} >
+            <ShinyButton >
+              <div className="flex flex-row items-center">
+                Get Started <MoveRight className="ml-2" size={16} />
+              </div>
+            </ShinyButton>
+          </BlurFade>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* <Logos8 /> */}
+      </div>
     </div>
   );
 }
---
+++ src/lib/consts.ts
@@ -0,0 +1,67 @@
+import { title } from "process";
+
+export const navigationItems = [
+    {
+        title: "Home",
+        href: "/",
+        description: "",
+    },
+    {
+        title: "About",
+        href: "/about",
+        description: "",
+    },
+    {
+        title: "Services",
+        href: "/services",
+        description: "",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+        description: "",
+    },
+    // {
+    //     title: "Community",
+    //     description: "Stay updated with the latest news and events.",
+    //     items: [
+    //         {
+    //             title: "Announcements",
+    //             href: "/announcements",
+    //         },
+    //         {
+    //             title: "Events",
+    //             href: "/events",
+    //         },
+    //         {
+    //             title: "Endowment Programs",
+    //             href: "/endowment-programs",
+    //         },
+    //         {
+    //             title: "Activities",
+    //             href: "/activities",
+    //         },
+    //     ],
+    // },
+    // {
+    //     title: "Hostel",
+    //     description: "Providing information and resources for hostel residents and visitors.",
+    //     items: [
+    //         {
+    //             title: "About us",
+    //             href: "/about",
+    //         },
+    //         {
+    //             title: "Facilities",
+    //             href: "/facilities",
+    //         },
+    //         {
+    //             title: "Contact us",
+    //             href: "/contact",
+    //         },
+    //     ],
+    // },
+];
+
+
+export const DB_NAME = "mea-trichur";
